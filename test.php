08bff271e65abf0262f6aa7ca533749633794e13
Christoph Fischer <chris@toph.de>
2014-01-31 15:00:24 +0100

    First text

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ $img = @imagecreatetruecolor(1024, 768) or die('Unable to create GD-stream');
 
 // some colors
 $color['white'] = imagecolorallocatealpha($img, 255, 255, 255, 127);
+$color['black'] = imagecolorallocatealpha($img, 0, 0, 0, 127);
 
 // fill image with white
 imagefill($img, 0, 0, $color['white']);
@@ -13,6 +14,9 @@ $img2 = imagecreatefromjpeg(dirname(__FILE__).'/test/test.jpg');
 
 // impose on the background image
 imagecopy($img, $img2, 0, 0, 0, 0, 1024, 507);
+
+// write first text
+imagetext ($img, 43, 0, 40, 540, $color['black'], dirname(__FILE__).'/fonts/OpenSans-Regular.ttf', 'Sonntag, 15:00 Uhr');
 
 
 
