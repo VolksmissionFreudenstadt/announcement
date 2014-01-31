d05e18027343f637ce58bb8c502ccb9ef0fced94
Christoph Fischer <chris@toph.de>
2014-01-31 14:49:32 +0100

    white background

--- test.php
+++ test.php
@@ -2,6 +2,12 @@
 
 $img = @imagecreatetruecolor(1024, 768) or die('Unable to create GD-stream');
 
+// some colors
+$color['white'] = imagecolorallocatealpha($img, 255, 255, 255, 127);
+
+// fill image with white
+imagefill($img, 0, 0, $color['white']);
+
 Header('Content-Type: image/jpeg');
 imagejpeg($img);
 
