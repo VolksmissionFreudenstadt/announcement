dafc1edf0f558a51e18f8a76d549a5c9b6e369b0
Christoph Fischer <chris@toph.de>
2014-01-31 14:53:39 +0100

    Copy title image

--- test.php
+++ test.php
@@ -8,6 +8,14 @@ $color['white'] = imagecolorallocatealpha($img, 255, 255, 255, 127);
 // fill image with white
 imagefill($img, 0, 0, $color['white']);
 
+// import title image:
+$img2 = imagecreatefromjpeg(dirname(__FILE__).'/test/test.jpg');
+
+// impose on the background image
+imagecopy($img, $img2, 0, 0, 0, 0, 1024, 507);
+
+
+
 Header('Content-Type: image/jpeg');
 imagejpeg($img);
 
