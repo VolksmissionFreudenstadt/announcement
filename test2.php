281638183963f9fc9cd4fbf69b08b5596be19cfd
Christoph Fischer <chris@toph.de>
2014-02-01 15:48:56 +0100

    Composite the two images

--- test2.php
+++ test2.php
@@ -4,6 +4,9 @@ $img = new Imagick();
 $img->newImage (1024, 768, new ImagickPixel('white'));
 $img->setImageFormat('jpeg');
 
+// copy test image onto background
+$img2 = new Imagick(dirname(__FILE__).'/test/test.jpg');
+$img->compositeImage($img2, Imagick::COMPOSITE_DEFAULT, 0, 0);
 
 header('Content-Type: image/jpeg');
 echo $img;
