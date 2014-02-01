5966138becfb53ced60e428272f3fcbebcd0da3a
Christoph Fischer <chris@toph.de>
2014-02-01 15:56:03 +0100

    First text

--- test2.php
+++ test2.php
@@ -8,5 +8,14 @@ $img->setImageFormat('jpeg');
 $img2 = new Imagick(dirname(__FILE__).'/test/test.jpg');
 $img->compositeImage($img2, Imagick::COMPOSITE_DEFAULT, 0, 0);
 
+// font settings
+$draw = new ImagickDraw();
+$draw->setFillColor('black');
+$draw->setFont(dirname(__FILE__).'/fonts/OpenSans-Regular.ttf');
+$draw->setFontSize(43);
+
+$img->annotateImage($draw, 40, 600, 0, 'Sonntag 15:00');
+
+
 header('Content-Type: image/jpeg');
 echo $img;
